7dfea40f49d79ec4d22a71a03fb8a3d6909c909e
Pavan Kumar N <pavankumar.n.46@gmail.com>
2019-10-26 19:30:08 +0530

    Lecture 1
    Design of basic architecture of the concentration game.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var flipCount = 0 {
+        didSet {
+            flipsLabel.text = "Flips : \(flipCount)"
+        }
     }
-
-
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet weak var flipsLabel: UILabel!
+    
+    let emojiList = ["👻","🎃","👻","🎃"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            print("Card number is \(cardNumber)")
+            flipCard(withEmoji: emojiList[cardNumber], on: sender)
+        } else {
+            print("Card was not selected")
+        }
+    }
+    
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
 }
 
